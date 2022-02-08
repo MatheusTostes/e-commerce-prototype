f515adb419eecc0df6d15f6b6876aec4cd44a71d
M. Tostes <64822140+MatheusTostes@users.noreply.github.com>
2022-02-08 18:24:43 -0300

    Create header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+<script src="https://unpkg.com/boxicons@2.1.1/dist/boxicons.js"></script>
 
 function App() {
   return (
     <div>
-        <p>
-          Hello World
-        </p>
+        <div className='header'>
+          <a className='logo' href="/">
+            <h1>Proto</h1>
+            <h1>TYPE</h1>
+          </a>
+          <div className='search-box'>
+            <box-icon name="search" size="1em" color="rgb(0, 216, 169)"></box-icon>
+            <input className='search-input' placeholder='Digite aqui o que quer encontrar!'/>
+          </div>
+          <div className='user-area'>
+            <a href='/'>
+              <box-icon name='cart' size="3em" color="rgb(243, 243, 243)"></box-icon>
+            </a>
+            <a href='/'>Entrar</a>
+          </div>
+        </div>
+
     </div>
   );
 }
