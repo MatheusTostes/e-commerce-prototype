30f6981015538d75e750a6cd35e675f4736ce29d
M. Tostes <64822140+MatheusTostes@users.noreply.github.com>
2022-02-09 18:19:00 -0300

    Create simple header

--- src/App.js
+++ src/App.js
@@ -6,38 +6,6 @@ function App() {
   return (
     <div>
       <Header/>
-        <div className='header'>
-          <a className='logo' href="/">
-            <h1>Proto</h1>
-            <h1>TYPE</h1>
-          </a>
-
-          <div className='search-box'>
-            <box-icon 
-              name="search" 
-              size="1em" 
-              color="rgb(0, 216, 169)">
-            </box-icon>
-
-            <input 
-              className='search-input' 
-              placeholder='Digite aqui o que quer encontrar!'
-            />
-            <button className='search-btn'>Buscar</button>
-          </div>
-
-          <div className='user-area'>
-            <a href='/'>
-              <box-icon 
-                name='cart' 
-                size="3em" 
-                color="rgb(243, 243, 243)">
-              </box-icon>
-            </a>
-            
-            <a href='/'>Entrar</a>
-          </div>
-        </div>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+    return (  
+        <div className="Container">
+            <div className='header'>
+          <a className='logo' href="/">
+            <h1>Proto</h1>
+            <h1>TYPE</h1>
+          </a>
+
+          <div className='search-box'>
+            <box-icon 
+              name="search" 
+              size="1em" 
+              color="rgb(0, 216, 169)">
+            </box-icon>
+
+            <input 
+              className='search-input' 
+              placeholder='Digite aqui o que quer encontrar!'
+            />
+            <button className='search-btn'>Buscar</button>
+          </div>
+
+          <div className='user-area'>
+            <a href='/'>
+              <box-icon 
+                name='cart' 
+                size="3em" 
+                color="rgb(243, 243, 243)">
+              </box-icon>
+            </a>
+            
+            <a href='/'>Entrar</a>
+          </div>
+        </div>
+        </div>
+    );
+}
+
+export default Header;
